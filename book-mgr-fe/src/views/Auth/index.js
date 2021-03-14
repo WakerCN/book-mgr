bb9429076bd13f59cc341abbe590d00d052d31e7
weiwei <3379950441@qq.com>
2021-03-14 14:15:49 +0800

    feat：完成前端登录注册页面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue";
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons-vue";
+
+export default defineComponent({
+  // 注册组件
+  components: {
+    UserOutlined,
+    LockOutlined,
+    MailOutlined
+  },
+  setup() {}
+});
