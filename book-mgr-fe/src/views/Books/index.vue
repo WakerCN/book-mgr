a3898955017ce009c28506806cfd88aafcbd0128
weiwei <3379950441@qq.com>
2021-03-18 10:19:01 +0800

    完成space-between组件 完成前端布局

--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Books</div>
+  <div>
+    <a-card>
+      <h2>图书管理系统</h2>
+      <a-divider></a-divider>
+      <space-between>
+        <a-input-search placeholder="根据书名搜索" enter-button class="search"></a-input-search>
+        <a-button type="primary">添加一条</a-button>
+      </space-between>
+    </a-card>
+    <a-table :dataSource="dataSource" :columns="columns"></a-table>
+  </div>
 </template>
 
 <script src="./index.js"></script>
